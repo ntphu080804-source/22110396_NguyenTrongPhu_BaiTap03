cb1e682baa06276f742f9b98eed1cd82fac39392
ntphu080804-source <ntphu080804@gmail.com>
2025-09-05 16:25:21 +0700

    mục 1

--- main/java/connection/DBConnection.java
+++ main/java/connection/DBConnection.java
@@ -1,22 +1,29 @@
 package connection;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+
 public class DBConnection {
+	private final String serverName = "LAPTOP-PUEORRNE\\SQLEXPRESS05";
+	private final String dbName = "QuanLyWeb";
+	private final String portNumber = "1433";
+	private final String userID = "sa";
+	private final String password = "213456789";
+
 	public Connection getConnection() throws Exception {
-		String serverName = "localhost";
-		String dbName = "ServletCRUDMVC";
-		String portNumber = "1433";
-		String instance = "";  
-		String userID = "sa";
-		String password = "123456";
-
-		String url = "jdbc:sqlserver://" + serverName + ":" + portNumber;
-		if (instance != null && !instance.trim().isEmpty()) {
-			url += "\\" + instance;
-		}
-		url += ";databaseName=" + dbName;
-
-		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		return DriverManager.getConnection(url, userID, password);
+        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber 
+                   + ";databaseName=" + dbName 
+                   + ";encrypt=true;trustServerCertificate=true;";
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
 	}
+	public static void main(String[] args) {
+		try {
+			System.out.println(new DBConnection().getConnection());
+			System.out.println("helloNha");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
